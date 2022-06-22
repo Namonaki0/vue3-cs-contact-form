3b70caabd48bfb4e93f393cdea096ddf05136bc0
Andre <andreferreira@live.co.uk>
2022-06-22 21:26:33 +0100

    first commit

---
+++ vue3-cs-contact-form/src/components/CSContactForm.vue
@@ -0,0 +1,322 @@
+<template>
+  <div class="flex-1 grid grid-cols-6 md:grid-cols-12 px-2 py-4" data-v-75749900="" data-v-567de64c-s="">
+    <div class="col-span-6 col-start-1 md:col-span-10 md:col-start-2 xl:col-span-10 xl:col-start-2 flex flex-col"
+      data-v-75749900="" data-v-567de64c-s="">
+      <span class="block overflow-hidden flex flex-col items-center text-center pb-6" data-v-75749900=""
+        data-v-567de64c-s=""><span
+          class="relative block">
+          <h1 class="heading-1" data-v-75749900="">Contact Us</h1>
+          <p class="text-md" data-v-75749900="">
+            If you have an enquiry regarding the online shop or a general
+            enquiry for our Customer Services department, please contact us
+            either by completing the form below.
+          </p>
+        </span></span>
+      <div class="" data-v-75749900=""
+        data-v-567de64c-s="">
+        <form class="grid flex-row w-full grid-cols-1 lg:grid-cols-2 md:gap-4" data-v-75749900="">
+          <div class="flex-grow pb-6" data-v-75749900="">
+            <div class="relative flex w-full">
+              <div
+                class="flex border-0 border-b border-solid border-black text-black w-full py-1 px-0">
+                <!----><input class="outline-none w-full" type="text" />
+              </div>
+              <span
+                class="absolute left-0 duration-300 pointer-events-none placeholder opacity-70 text-black ">First
+                Name*</span>
+              <!---->
+              <!---->
+              <div class="w-0 bg-black h-divider"></div>
+            </div>
+            <!---->
+          </div>
+          <div class="flex-grow pb-6" data-v-75749900="">
+            <div class="relative flex w-full">
+              <div
+                class="flex border-0 border-b border-solid border-black py-1 text-black w-full py-1 px-0">
+                <!----><input class="outline-none w-full" type="text" />
+              </div>
+              <span
+                class="absolute left-0 pointer-events-none placeholder opacity-70 bottom-1 text-black">Last
+                Name*</span>
+              <!---->
+              <!---->
+              <div class="w-0 bg-black h-divider"></div>
+            </div>
+            <!---->
+          </div>
+          <div class="flex-grow pb-6" data-v-75749900="">
+            <div class="relative flex w-full">
+              <div
+                class="flex border-0 border-b border-solid border-black py-1 text-black w-full py-1 px-0">
+                <!----><input class="outline-none w-full" type="email" />
+              </div>
+              <span
+                class="absolute left-0 pointer-events-none placeholder opacity-70 bottom-1 text-black">Email
+                Address*</span>
+              <!---->
+              <!---->
+              <div class="w-0 bg-black h-divider"></div>
+            </div>
+            <!---->
+          </div>
+          <div class="relative flex-grow pb-6" data-v-75749900="">
+            <div class="flex items-center w-full" data-v-12419bc9="">
+              <!---->
+              <div class="relative select-input w-full cursor-pointer" data-v-12419bc9="">
+                <label class="absolute text-base bottom-1 left-0 opacity-60 transform duration-400 ease-ease-1"
+                  data-v-12419bc9="">Enquiry type*</label>
+                <div
+                  class="transform absolute w-2 h-2 -translate-y-1/2 right-0.5 top-1/2 transform transform-all ease-ease-1 duration-300"
+                  data-v-d79371a0="" data-v-12419bc9="" style="width: 10px; height: 10px">
+                  <div class="icon" data-v-d79371a0="" style="
+                      --fillColour: none;
+                      --strokeColour: black;
+                      --width: 10px;
+                      --height: 10px;
+                     ;
+                    "></div>
+                </div>
+                <input class="pointer-events-none text-black w-full py-1 px-0 outline-none"
+                  type="select" data-v-12419bc9="" />
+                <div class="w-full bg-black h-divider" data-v-12419bc9=""></div>
+              </div>
+              <!--? OPTIONS DROPDOWN -->
+              <ul
+                class="select-options fixed z-100 h-200 overflow-y-auto text-sm border border-b-0 border-l-0 border-solid pointer-events-auto bg-lightGrey border-grey ps-scroll"
+                data-v-12419bc9="" style="width: 484px; top: 177px; left: 633px;">
+                <!---->
+                <li class="relative block p-2 bg-lightGrey dropdown-item cursor-pointer" data-v-12419bc9="" style="">
+                  <div class="flex flex-col" data-v-12419bc9="">
+                    <div class="flex items-center" data-v-12419bc9="">
+                      <!---->Deliveries
+                    </div>
+                    <!---->
+                  </div>
+                </li>
+                <li class="relative block p-2 bg-grey dropdown-item cursor-pointer" data-v-12419bc9="" style="">
+                  <div class="flex flex-col" data-v-12419bc9="">
+                    <div class="flex items-center" data-v-12419bc9="">
+                      <!---->Orders
+                    </div>
+                    <!---->
+                  </div>
+                </li>
+                <li class="relative block p-2 bg-lightGrey dropdown-item cursor-pointer" data-v-12419bc9="">
+                  <div class="flex flex-col" data-v-12419bc9="">
+                    <div class="flex items-center" data-v-12419bc9="">
+                      <!---->Payments
+                    </div>
+                    <!---->
+                  </div>
+                </li>
+                <li class="relative block p-2 bg-grey dropdown-item cursor-pointer" data-v-12419bc9="">
+                  <div class="flex flex-col" data-v-12419bc9="">
+                    <div class="flex items-center" data-v-12419bc9="">
+                      <!---->Product/Stock
+                    </div>
+                    <!---->
+                  </div>
+                </li>
+                <li class="relative block p-2 bg-lightGrey dropdown-item cursor-pointer" data-v-12419bc9="">
+                  <div class="flex flex-col" data-v-12419bc9="">
+                    <div class="flex items-center" data-v-12419bc9="">
+                      <!---->Returns
+                    </div>
+                    <!---->
+                  </div>
+                </li>
+                <li class="relative block p-2 bg-grey dropdown-item cursor-pointer" data-v-12419bc9="">
+                  <div class="flex flex-col" data-v-12419bc9="">
+                    <div class="flex items-center" data-v-12419bc9="">
+                      <!---->Shipping
+                    </div>
+                    <!---->
+                  </div>
+                </li>
+                <li class="relative block p-2 bg-lightGrey dropdown-item cursor-pointer" data-v-12419bc9="">
+                  <div class="flex flex-col" data-v-12419bc9="">
+                    <div class="flex items-center" data-v-12419bc9="">
+                      <!---->Promotion
+                    </div>
+                    <!---->
+                  </div>
+                </li>
+                <li class="relative block p-2 bg-grey dropdown-item cursor-pointer" data-v-12419bc9="">
+                  <div class="flex flex-col" data-v-12419bc9="">
+                    <div class="flex items-center" data-v-12419bc9="">
+                      <!---->Website/Technical
+                    </div>
+                    <!---->
+                  </div>
+                </li>
+              </ul>
+              <!--? OPTIONS DROPDOWN - END -->
+              <!---->
+            </div>
+            <!---->
+          </div>
+          <!---->
+          <div class="flex-grow lg:col-span-2 pb-6" data-v-75749900="">
+            <div class="relative placeholder-colour-fix" data-v-54de40c9="" data-v-75749900=""
+              style="--225913de: 220px">
+              <textarea placeholder="Write your message here*"
+                class="outline-none text-black text-sm border border-solid border-black font-medium w-full p-2 h-48 placeholder:text-slate-700"
+                maxlength="999999" data-v-54de40c9=""></textarea>
+              <!---->
+              <!---->
+            </div>
+          </div>
+          <div class="flex-grow pb-6 lg:col-start-2" data-v-75749900="">
+            <button
+              class="flex items-center transition-all duration-400 ease-1 disabled:opacity-30 disabled:cursor-not-allowed primary font-medium text-sm font-medium text-white p-1 py-3 px-2 mx-0 justify-self-end flex-1 w-full justify-between"
+              type="button" data-v-75749900="">
+              <div class="relative order-1">
+                <span class="label whitespace-nowrap">Submit</span>
+                <!---->
+              </div>
+              <div class="-rotate-45 order-2">
+                <div class="animate-spin-progress" data-v-75749900="">
+                  <!---->
+                </div>
+              </div>
+              <!---->
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+
+*,
+:after,
+:before {
+  --tw-ring-inset: var(--tw-empty);
+  --tw-ring-offset-width: 0px;
+  --tw-ring-offset-color: #fff;
+  --tw-ring-color: rgba(59, 130, 246, .5);
+  --tw-ring-offset-shadow: 0 0 #0000;
+  --tw-ring-shadow: 0 0 #0000;
+}
+
+*,
+:after,
+:before {
+  --tw-shadow: 0 0 #0000;
+}
+
+*,
+:after,
+:before {
+  border-color: currentColor;
+}
+
+*,
+:after,
+:before {
+  border: 0 solid;
+  box-sizing: border-box;
+}
+
+body {
+  display: grid;
+  color: 
+}
+
+body,
+h1 {
+  margin: 0;
+  padding: 0;
+  display: block;
+  font-size: 2em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  font-weight: strong;
+}
+.heading-1 {
+  font-size: min(max(42px, 14vw), 86px);
+  font-weight: 500;
+  letter-spacing: 0.01em;
+}
+
+.text-md {
+  font-size: 20px;
+}
+
+.font-medium {
+  font-weight: 500;
+}
+
+.px-2 {
+  padding-left: 16px;
+  padding-right: 16px;
+}
+
+.py-4 {
+  padding-bottom: 32px;
+  /* padding-top: 32px; */
+}
+
+
+.flex-1 {
+  flex: 1 1 0%;
+}
+
+.pb-6 {
+  padding-bottom: 48px;
+}
+
+.flex-grow {
+  flex-grow: 1;
+}
+
+input,
+textarea {
+  color: #000000;
+  background: inherit;
+}
+
+
+
+ul {
+  visibility: hidden;
+}
+
+/* span["bottom"]:focus {
+  
+} */
+
+element.style {
+  --225913de: 220px;
+}
+
+.relative {
+  position: relative;
+}
+
+.border-error,
+.border-red {
+  --tw-border-opacity: 1;
+  border-color: rgba(228, 69, 72, var(--tw-border-opacity));
+}
+
+button {
+  background: #000000;
+}
+
+/* @media (min-width: 768px) .md\:grid-cols-12 {
+  grid-template-columns: repeat(12, minmax(0, 1fr));
+} */
+</style>
